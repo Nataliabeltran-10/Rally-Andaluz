34856888329bf957dd6edf8f99f523f00109325b
Nataliabeltran-10 <145378881+Nataliabeltran-10@users.noreply.github.com>
2025-04-06 11:46:56 +0200

    .

--- registro_participante/conexion.php
+++ registro_participante/conexion.php
@@ -1,9 +1,9 @@
 <?php
 // Configuración de la conexión a la base de datos
 $host = "PMYSQL182.dns-servicio.com";  // Cambia si tu base de datos está en otro servidor
-$dbname = "10868095_rally_andaluz";  // Reemplaza con el nombre de tu base de datos
-$username = "natalia";  // Usuario de la base de datos
-$password = "";  // Contraseña (si tienes una)
+$dbname = "10868095_RallyAndaluz";  // Reemplaza con el nombre de tu base de datos
+$username = "Rally";  // Usuario de la base de datos
+$password = "PR0ye(tOR@ly";  // Contraseña (si tienes una)
 
 // Intenta la conexión con PDO
 try {
