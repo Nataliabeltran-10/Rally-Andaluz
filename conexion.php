391d4486215ef6fe0cc1d986e4ae44567b01a67d
Nataliabeltran-10 <145378881+Nataliabeltran-10@users.noreply.github.com>
2025-04-14 12:38:21 +0200

    .

--- conexion.php
+++ conexion.php
@@ -1,8 +1,6 @@
 <?php
-$host = "PMYSQL182.dns-servicio.com";  // Cambia si tu base de datos está en otro servidor
-$dbname = "10868095_RallyAndaluz";  // Reemplaza con el nombre de tu base de datos
-$username = "Rally";  // Usuario de la base de datos
-$password = "PR0ye(tOR@ly";  // Contraseña (si tienes una)
+$conexion = mysqli_connect("PMYSQL182.dns-servicio.com", "Rally", "PR0ye(tOR@ly", "10868095_RallyAndaluz");
+
 
 if (!$conexion) {
   die("Error al conectar con la base de datos: " . mysqli_connect_error());
